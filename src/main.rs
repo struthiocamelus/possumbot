7591ffe55df902ca9a326a978b79b9fa4604f0e4
Sistercities <sistercities@disroot.org>
2019-05-15 04:04:58 -0400

    move to .env, add possum command

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,46 @@
-#[macro_use]
 extern crate serenity;
+extern crate giphy;
+extern crate reqwest;
+extern crate dotenv;
 
-extern crate config;
-
-use serenity::client::Client;
-use serenity::prelude::EventHandler;
-use serenity::framework::standard::StandardFramework;
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+use giphy::v1::sync::*;
+use giphy::v1::gifs::RandomRequest;
+use dotenv::dotenv;
+use std::env;
 
 struct Handler;
 
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+	fn message(&self, _: Context, msg: Message) {
+		if msg.content == "!possum" {
+			let http_client = reqwest::Client::new();
+			let giphy_api = SyncApi::new(env::var("POSSUM_GIPHY_TOKEN").unwrap(), http_client);
+
+			let response = RandomRequest::new().with_tag("possum")
+				.send_to(&giphy_api)
+				.unwrap_or_else(|e| panic!("Error while calling search endpoint: {:?}", e));
+
+			if let Err(why) = msg.reply(&response.data.embed_url) {
+				println!("Error sending message: {:?}", why);
+			}
+		}
+	}
+
+	fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+	}
+}
 
 fn main() {
-	let mut settings = config::Config::default();
-	settings.merge(config::File::with_name("possumconfig")).unwrap();
+	dotenv().ok();
 
-	let mut client = Client::new(&settings.get_str("discord.token").unwrap(), Handler)
-		.expect("Error creating client");
-	client.with_framework(StandardFramework::new()
-						  .configure(|c| c.prefix(&settings.get_str("discord.prefix").unwrap()))
-						  .cmd("ping", ping));
-	
+	let mut client = Client::new(&env::var("POSSUM_DISCORD_TOKEN").unwrap(), Handler).expect("Error creating client");
+
 	if let Err(why) = client.start() {
 		println!("An error occurred while running the client: {:?}", why);
 	}
 }
-
-command!(ping(_context, msg) {
-	let _ = msg.reply("pong!");
-});
